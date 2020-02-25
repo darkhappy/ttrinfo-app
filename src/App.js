e5b33a369e93a6c56ffe838bffdaebccd8a0f52e
DarkHappyy <57161803+DarkHappyy@users.noreply.github.com>
2020-02-24 21:48:48 -0500

    managed to get data BOYS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootswatch/dist/darkly/bootstrap.min.css";
+import axios from "axios";
 
 import Population from "./components/Population.jsx";
 import Header from "./components/Header.jsx";
@@ -7,27 +8,78 @@ import Invasions from "./components/Invasions.jsx";
 import ServerStatus from "./components/ServerStatus.jsx";
 import SillyMeter from "./components/SillyMeter.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div class="row">
-        <div class="col-5">
-          <Invasions />
-        </div>
-        <div class="col-7">
-          <Population />
-        </div>
-        <div className="w-100" />
-        <div class="col-8">
-          <SillyMeter />
-        </div>
-        <div class="col-4">
-          <ServerStatus />
+class App extends Component {
+  state = {
+    invData: [],
+    popData: [],
+    sillyData: []
+  };
+
+  async componentDidMount() {
+    try {
+      setInterval(async () => {
+        // define variables
+        var popData;
+        var sillyData;
+        var invData;
+
+        // start with population
+        await axios({
+          method: "get",
+          url: "https://www.toontownrewritten.com/api/population"
+        }).then(resp => {
+          popData = resp.data;
+        });
+
+        // head out to the sillymeter now
+        await axios({
+          method: "get",
+          url: "https://www.toontownrewritten.com/api/sillymeter"
+        }).then(resp => {
+          sillyData = resp.data;
+        });
+        // quick stop at the invasions
+        await axios({
+          method: "get",
+          url: "https://www.toontownrewritten.com/api/invasions"
+        }).then(resp => {
+          invData = resp.data;
+        });
+
+        // alright slap that shit in states
+        this.setState({
+          popData: popData,
+          invData: invData,
+          sillyData: sillyData
+        });
+      }, 5000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="row">
+          <div className="col-5">
+            <Invasions invData={this.state.invData} />
+          </div>
+          <div className="col-7">
+            <Population popData={this.state.popData} />
+          </div>
+          <div className="w-100" />
+          <div className="col-8">
+            <SillyMeter sillyData={this.state.sillyData} />
+          </div>
+          <div className="col-4">
+            <ServerStatus />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
