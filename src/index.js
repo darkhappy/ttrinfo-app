119b6077cd56b1ff974d5510f95b422a54beee05
DarkHappyy <57161803+DarkHappyy@users.noreply.github.com>
2020-03-02 20:02:12 -0500

    made it so you can see stuff when the app launchs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
