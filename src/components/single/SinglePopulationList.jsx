1bb591e07bf89eb1f6f781af9dec2820b0aa1d90
darkhappy <darkhappyy@gmail.com>
2020-03-28 23:07:42 -0400

    SC only tag for pops

--- src/components/single/SinglePopulationList.jsx
+++ src/components/single/SinglePopulationList.jsx
@@ -5,6 +5,15 @@ class SinglePopulationList extends Component {
   formatValue = value => value.toFixed(0);
   duration = 500;
 
+  speedchat(district) {
+    console.log(district);
+    const speedchatOnly = ["Gulp Gulch", "Boingbury", "Whoosh Rapids"];
+
+    return speedchatOnly.includes(district) ? (
+      <span className="badge badge-success">SC Only</span>
+    ) : null;
+  }
+
   populationList() {
     const { list } = this.props;
     const { formatValue, duration } = this;
@@ -13,7 +22,7 @@ class SinglePopulationList extends Component {
       <ul className="list-group list-group-flush text-right text-muted small">
         {list.map(item => (
           <li key={item.name} className="list-group-item">
-            <strong>{item.name}:</strong>{" "}
+            {this.speedchat(item.name)} <strong>{item.name}:</strong>{" "}
             <AnimatedNumber
               value={item.population}
               formatValue={formatValue}
